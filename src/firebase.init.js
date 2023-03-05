bd13b38669e13e92304aada5145f5bca7df644ed
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-03-05 19:57:01 +0600

    email password authentication and simple form

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCkRh4X87GLxDwj_mHwLH8L_89kcLdTQw",
+  authDomain: "email-password-authen-ebb45.firebaseapp.com",
+  projectId: "email-password-authen-ebb45",
+  storageBucket: "email-password-authen-ebb45.appspot.com",
+  messagingSenderId: "462517234752",
+  appId: "1:462517234752:web:7de727154b4620597e03a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
